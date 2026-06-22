c2b3a529fc326acffa02498ec7f646ea0ce5708b
daniljromero-lgtm <danil.j.romero@gmail.com>
2026-06-21 21:15:31 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,22 @@
+const CACHE_NAME = "companero-v1";
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./css/styles.css",
+  "./js/app.js"
+];
 
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
